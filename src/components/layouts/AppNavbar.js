be0032ddaa2c7131163421119769fb136fa64477
katiedodd <ktdodd511@gmail.com>
2019-03-25 15:48:17 -0400

    initial commit

---
+++ src/components/layouts/AppNavbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppNavbar extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default AppNavbar;
