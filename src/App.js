178c662ccfaf67fd6d4b796ae7c7d5a34c425610
katiedodd <ktdodd511@gmail.com>
2019-03-25 16:29:58 -0400

    created navbar and clients page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavbar from './components/layouts/AppNavbar';
+import Dashboard from './components/layouts/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
