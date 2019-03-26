f3ba8fa7cf57d32d696f3bf0e5cf699418a6b56f
katiedodd <ktdodd511@gmail.com>
2019-03-26 16:13:05 -0400

    set auth with firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -7,7 +8,10 @@ import store from './store';
 import AppNavbar from './components/layouts/AppNavbar';
 import Dashboard from './components/layouts/Dashboard';
 import AddClient from './components/clients/AddClient';
+import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
+import Login from './components/auth/Login';
+
 
 class App extends Component {
   render() {
@@ -18,9 +22,11 @@ class App extends Component {
             <AppNavbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+                <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+                <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+                <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
               </Switch>
             </div>
           </div>
--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -8,8 +8,66 @@ import Spinner from '../layouts/Spinner';
 import classnames from 'classnames';
 
 class ClientDetails extends Component {
+  state = {
+    showBalanceUpdate: false,
+    balanceUpdateAmount: ''
+  }
+
+  // update balance
+  balanceSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore } = this.props;
+    const { balanceUpdateAmount } = this.state;
+
+    const clientUpdate = {
+      balance: parseFloat(balanceUpdateAmount)
+    }
+
+    // update in firestore
+    firestore.update({collection: 'clients', doc: client.id}, clientUpdate);
+  }
+
+  onDeleteClick = () => {
+    const { client, firestore, history } = this.props;
+
+    firestore.delete({collection: 'clients', doc: client.id})
+      .then(history.push('/'));
+  }
+
+  onChange = e => this.setState({[e.target.name]: e.target.value});
+
   render() {
     const { client } = this.props;
+    const { showBalanceUpdate, balanceUpdateAmount } = this.state;
+
+    let balanceForm = '';
+    // if balance form show display
+    if(showBalanceUpdate) {
+      balanceForm = (
+        <form onSubmit={this.balanceSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              name="balanceUpdateAmount"
+              placeholder="Add New Balance"
+              value={balanceUpdateAmount}
+              onChange={this.onChange}
+            />
+            <div class="input-group-append">
+              <input
+                type="submit"
+                value="Update"
+                className="btn btn-outline-dark"
+              />
+            </div>
+          </div>
+        </form>
+      )
+    } else {
+      balanceForm = null;
+    }
 
     if(client) {
       return (
@@ -25,7 +83,7 @@ class ClientDetails extends Component {
                 <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
                   Edit
                 </Link>
-                <button className="btn btn-danger">
+                <button onClick={this.onDeleteClick} className="btn btn-danger">
                   Delete
                 </button>
               </div>
@@ -44,12 +102,21 @@ class ClientDetails extends Component {
                 </div>
                 <div className="col-md-4 col-sm-6">
                   <h3 className="pull-right">
-                    Balance: <span className={classnames({
+                    Balance:{' '}
+                    <span className={classnames({
                       'text-danger': client.balance > 0,
                       'text-success': client.balance === 0
-                    })}>${parseFloat(client.balance).toFixed(2)}</span>
+                    })}>
+                      ${parseFloat(client.balance).toFixed(2)}
+                    </span>
+                    {' '}
+                    <small>
+                      <a href="#!" onClick={() => this.setState({ showBalanceUpdate: !this.state.showBalanceUpdate })}>
+                        <i className="fas fa-pencil-alt"></i>
+                      </a>
+                    </small>
                   </h3>
-                  {/* @TODO - balance form */}
+                  {balanceForm}
                 </div>
               </div>
 
